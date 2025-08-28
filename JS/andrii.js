c417bdf0595a9de180eb73fdeebaca7b987f07d8
kaktys3 <149934318+kaktys3@users.noreply.github.com>
2025-08-28 15:49:59 +0300

    add kactus

--- JS/andrii.js
+++ JS/andrii.js
@@ -1,14 +1,29 @@
 let canv = document.querySelector('.canvas')
 let canvModel = canv.getContext('2d')
 
-let dinoMiniElem = document.createElement('img')
-let dinoBigElem = document.createElement('img')
-let dinoThreeElem = document.createElement('img')
+const dinoGame = () => {
+    let dinoMiniElem = new Image()
+    let dinoBigElem = new Image()
+    let dinoThreeElem = new Image()
 
-let timer = Math.round(Math.random() * (1000 - 1500) + 1500)
-console.log(timer)
+    dinoMiniElem.src = '../andrii img/kaktus-mini.png'
+    dinoBigElem.src = '../andrii img/kaktus-big.png'
+    dinoThreeElem.src = '../andrii img/many-kactus.png'
 
-dinoMiniElem.src = ('/andrii img/kaktus-mini.png')
-dinoBigElem.src = ('/andrii img/kaktus-big.png')
-dinoThreeElem.src = ('/andrii img/many-kactus.png')
+    let timer = Math.round(Math.random() * (1000 - 1500) + 1500)
+    console.log(timer)
 
+    dinoBigElem.onload = () => {
+        canvModel.drawImage(dinoBigElem, 275, 110, 20, 40);
+    };
+
+    dinoMiniElem.onload = () => {
+        canvModel.drawImage(dinoMiniElem, 255, 130, 10, 20);
+    };
+
+    dinoThreeElem.onload = () => {
+        canvModel.drawImage(dinoThreeElem, 220, 110, 20, 40);
+    };
+}
+
+console.log(dinoGame())
