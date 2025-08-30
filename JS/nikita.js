4c3e9e3a3be1ea2799930c6b99d3b0f01415a7c2
olha36 <olhakucheruk36@gmail.com>
2025-08-30 18:43:11 +0300

    add image, make function of a global scope

--- JS/nikita.js
+++ JS/nikita.js
@@ -1,20 +1,21 @@
- function checkYear() {
-            const year = document.getElementById("yearInput").value;
-            const result = document.getElementById("result");
+function checkYear() {
+  const year = document.getElementById("yearInput").value;
+  const result = document.getElementById("result");
 
-            if (!year) {
-                result.innerHTML = "Будь ласка, введіть рік!";
-                result.className = "not-leap";
-                return;
-            }
+  if (!year) {
+    result.innerHTML = "Будь ласка, введіть рік!";
+    result.className = "not-leap";
+    return;
+  }
 
-            const isyear = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+  const isyear = (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 
-            if (isyear) {
-                result.innerHTML = "Ви народилися у високосний рік!";
-                result.className = "leap";
-            } else {
-                result.innerHTML = "Ви народилися не у високосний рік!";
-                result.className = "not-leap";
-            }
-        }
+  if (isyear) {
+    result.innerHTML = "Ви народилися у високосний рік!";
+    result.className = "leap";
+  } else {
+    result.innerHTML = "Ви народилися не у високосний рік!";
+    result.className = "not-leap";
+  }
+}
+window.checkYear = checkYear;
