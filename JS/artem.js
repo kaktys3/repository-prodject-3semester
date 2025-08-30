a5078cff1e953da151d44869b4f74cacb6977816
nasococos0987-artempro <proartemkuza@gmail.com>
2025-08-30 11:21:46 +0300

    Project is    done

--- JS/artem.js
+++ JS/artem.js
@@ -1,1 +1,12 @@
-console.log('Artem');
+ const randomNumber = Math.floor(Math.random() * 10) + 1;
+    function checkNumber() {
+      const userNumber = document.getElementById('userNumber').value;
+      const result = document.getElementById('result');
+      if (parseInt(userNumber) === randomNumber) {
+        result.textContent = `Вітаю, ви вгадали число ${randomNumber}!`;
+        result.className = "win";
+      } else {
+        result.textContent = `Ви програли, комп'ютер загадав ${randomNumber}`;
+        result.className = "lose";
+      }
+    }
