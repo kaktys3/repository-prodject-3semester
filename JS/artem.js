5a1cedb7a562ef534b37871c574146c97ad4015b
olha36 <olhakucheruk36@gmail.com>
2025-08-30 18:50:34 +0300

    fix guess the number logic so that it works without refreshing

--- JS/artem.js
+++ JS/artem.js
@@ -1,12 +1,18 @@
- const randomNumber = Math.floor(Math.random() * 10) + 1;
-    function checkNumber() {
-      const userNumber = document.getElementById('userNumber').value;
-      const result = document.getElementById('result');
-      if (parseInt(userNumber) === randomNumber) {
-        result.textContent = `Вітаю, ви вгадали число ${randomNumber}!`;
-        result.className = "win";
-      } else {
-        result.textContent = `Ви програли, комп'ютер загадав ${randomNumber}`;
-        result.className = "lose";
-      }
-    }
+const userNumber = document.getElementById("userNumber").value;
+const result = document.getElementById("result");
+
+function getRandomNumber() {
+  const randomNumber = Math.floor(Math.random() * 10) + 1;
+  return randomNumber;
+}
+
+function checkNumber() {
+  if (parseInt(userNumber) === getRandomNumber()) {
+    result.textContent = `Вітаю, ви вгадали число ${getRandomNumber()}!`;
+    result.className = "win";
+  } else {
+    result.textContent = `Ви програли, комп'ютер загадав ${getRandomNumber()}`;
+    result.className = "lose";
+  }
+}
+window.checkNumber = checkNumber;
