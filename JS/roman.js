733e785a8266216f386def8d24ecfb2fde050512
TVOROG5776 <romansydorko10@gmail.com>
2025-08-30 11:28:32 +0300

    calculator-project

--- JS/roman.js
+++ JS/roman.js
@@ -1,1 +1,57 @@
-console.log('Roman');
+const inputOne = document.querySelector("#number-one");
+const additionNumber = document.querySelector("#addition");
+const multiplicationNumber = document.querySelector("#multiplication");
+const minusNumber = document.querySelector("#minus");
+const divisionNumber = document.querySelector("#division");
+const inputTwo = document.querySelector("#number-two");
+const numberResult = document.querySelector("#result");
+const formCalculate = document.querySelector(".calculate-form");
+
+const actions = document.querySelector(".calculate-action");
+
+let currentOperation = 'number';
+
+function addition() {
+  currentOperation = "addition";
+}
+
+function multiplication() {
+  currentOperation = "multiplication";
+}
+
+function subtraction() {
+  currentOperation = "subtraction";
+}
+
+function division() {
+  currentOperation = "division";
+}
+
+function calculatorActions () {
+  if (currentOperation === "addition") {
+    let result = Number(inputOne.value.trim()) + Number(inputTwo.value.trim());
+    numberResult.textContent = result;
+  }
+  if (currentOperation === "multiplication") {
+    let result = Number(inputOne.value.trim()) * Number(inputTwo.value.trim());
+    numberResult.textContent = result;
+  }
+  if (currentOperation === "subtraction") {
+    let result = Number(inputOne.value.trim()) - Number(inputTwo.value.trim());
+    numberResult.textContent = result;
+  }  
+  if (currentOperation === "division") {
+    let result = Number(inputOne.value.trim()) / Number(inputTwo.value.trim());
+    numberResult.textContent = result;
+  }  
+}
+
+additionNumber.addEventListener("click",  addition);
+
+multiplicationNumber.addEventListener("click", multiplication);
+
+minusNumber.addEventListener("click", subtraction);
+
+divisionNumber.addEventListener("click", division);
+
+formCalculate.addEventListener("click", calculatorActions);
