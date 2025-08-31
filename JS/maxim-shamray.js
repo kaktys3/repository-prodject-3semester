eabc4ad23ae8a59b84365dd5fb9b6a86899a644a
Maksym Shamrai <150514219+maksymshamrai@users.noreply.github.com>
2025-08-31 11:08:17 +0200

    js base

--- JS/maxim-shamray.js
+++ JS/maxim-shamray.js
@@ -1,1 +1,13 @@
 console.log('Maksym Shamrai');
+
+let compScore = document.querySelector(".rps-result-computer")
+let userScore = document.querySelector(".rps-result-player")
+let showWinner = document.querySelector(".rps-won")
+let showComputerChoice = document.querySelector(".rps-comp-variant")
+
+let defaultUserScore = 0
+let defaultComputerScore = 0
+
+function increaseComputerScore () {}
+function increaseUserScore () {}
+function showComputerVariant () {}
