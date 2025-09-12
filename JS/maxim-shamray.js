84e5e042d9e8de2cd898a02e908e63b626d998f8
Maksym Shamrai <150514219+maksymshamrai@users.noreply.github.com>
2025-09-12 22:32:36 +0200

    js base 2

--- JS/maxim-shamray.js
+++ JS/maxim-shamray.js
@@ -1,13 +1,32 @@
 console.log('Maksym Shamrai');
 
-let compScore = document.querySelector(".rps-result-computer")
-let userScore = document.querySelector(".rps-result-player")
-let showWinner = document.querySelector(".rps-won")
-let showComputerChoice = document.querySelector(".rps-comp-variant")
+let compScore = document.querySelector(".rps-result-computer");
+let userScore = document.querySelector(".rps-result-player");
+let showWinner = document.querySelector(".rps-won");
+let showComputerChoice = document.querySelector(".rps-comp-variant");
 
-let defaultUserScore = 0
-let defaultComputerScore = 0
+let defaultUserScore = 0;
+let defaultComputerScore = 0;
 
-function increaseComputerScore () {}
-function increaseUserScore () {}
-function showComputerVariant () {}
+let userChoice;
+let compChoice;
+
+document.querySelector(".rock").onclick = function() {
+    // console.log("Вибрано камінь");
+    userChoice = "rock"
+    console.log(userChoice);
+}
+document.querySelector(".paper").onclick = function() {
+    // console.log("Вибрано папір");
+    userChoice = "paper"
+    console.log(userChoice);
+}
+document.querySelector(".scissors").onclick = function() {
+    // console.log("Вибрано ножиці");
+    userChoice = "scissors"
+    console.log(userChoice);
+}
+
+function increaseComputerScore() {}
+function increaseUserScore() {}
+function showComputerVariant() {}
